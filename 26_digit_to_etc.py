ea0ef62f451c472928fbaff28d092dc6ecada5af
fizzahwaseem <fizawaseem66@gmail.com>
2019-06-29 21:46:13 +0500

    Uploaded file

---
+++ 26_digit_to_etc.py
@@ -0,0 +1,5 @@
+#Python program to convert an integer to Binary, Octal and Hexadecimal numbers
+decimal = int(input("Enter an integer: "))
+print(decimal, "to binary: ", bin(decimal))
+print(decimal, "to octal: ", oct(decimal))
+print(decimal, "to hexadecimal: ", hex(decimal))
